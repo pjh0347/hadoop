443dbde1373f6974bede2e59bf2e6f03bc26e43b
Jinho Park <pjh0347@gmail.com>
2017-10-19 18:11:16 +0900

    updated hbase query.

--- hbase/loader/script/query.sh
+++ hbase/loader/script/query.sh
@@ -1,8 +1,14 @@
 
+describe 'apache_access_log'
+
 get 'apache_access_log', '<yyyyMMddHHmmss>-<client address>'
 get 'apache_access_log', '<yyyyMMddHHmmss>-<client address>', { COLUMN => 'client' }
 get 'apache_access_log', '<yyyyMMddHHmmss>-<client address>', { COLUMN => 'client:address' }
 get 'apache_access_log', '<yyyyMMddHHmmss>-<client address>', { COLUMN => 'client:address', VERSIONS => 100 }
 get 'apache_access_log', '<yyyyMMddHHmmss>-<client address>', { FILTER => "ValueFilter(=, 'binary:<any value>')" }
-get 'apache_access_log', '<yyyyMMddHHmmss>-<client address>', { ROWPREFIXFILTER => '<yyyyMMddHHmmss>' }
-get 'apache_access_log', '<yyyyMMddHHmmss>-<client address>', { ROWPREFIXFILTER => '<yyyyMMdd>' }
+
+scan 'apache_access_log', ROWPREFIXFILTER => '<yyyyMMddHHmmss>'
+scan 'apache_access_log', ROWPREFIXFILTER => '<yyyyMMddHHmmss>', COLUMNS => 'request'
+scan 'apache_access_log', ROWPREFIXFILTER => '<yyyyMMddHHmmss>', COLUMNS => 'request:url'
+scan 'apache_access_log', ROWPREFIXFILTER => '<yyyyMMddHHmmss>-<client address>', COLUMNS => 'request:url', VERSIONS => 1000
+scan 'apache_access_log', ROWPREFIXFILTER => '<yyyyMMddHHmmss>', COLUMNS => 'request:url', VERSIONS => 1000, FILTER => "ValueFilter(=, 'substring:.jpg')"
